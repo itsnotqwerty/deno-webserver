1594b9290b1189926c0ead14c8c0093a0ec87219
Sam <samroux808@gmail.com>
2023-06-11 18:40:47 -0500

    fixed casing

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,7 +1,7 @@
 import { React } from "../dep.ts";
 import Comet from "./Comet.tsx";
 import Orbit from "./Orbit.tsx";
-import Ufo from "./UFO.tsx";
+import Ufo from "./Ufo.tsx";
 
 const App = () => {
     return <div id="root">
