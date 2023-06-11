96770a14a24f21e373bae87ae4f865bba2d8e88e
Sam <samroux808@gmail.com>
2023-06-11 17:19:29 -0500

    Added astronaut

---
+++ frontend/components/Astronaut.tsx
@@ -0,0 +1,7 @@
+import { React } from "../dep.ts";
+
+const Astronaut = () => {
+    return <img src="/static/images/astronaut.png" id="astronaut" alt="astronaut" />
+}
+
+export default Astronaut;
--- frontend/components/Orbit.tsx
+++ frontend/components/Orbit.tsx
@@ -1,9 +1,11 @@
 import { React } from "../dep.ts";
 import Earth from "./Earth.tsx";
 import Alien from "./Alien.tsx";
+import Astronaut from "./Astronaut.tsx";
 
 const Orbit = () => {
     return <div id="orbit">
+        <Astronaut />
         <Earth /> <Alien />
     </div>
 }
