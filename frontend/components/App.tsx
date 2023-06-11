c1fc67104ef38d3642198a2924f077cca0575ae8
Sam <samroux808@gmail.com>
2023-06-11 15:22:38 -0500

    Added UFO

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,9 +1,13 @@
 import { React } from "../dep.ts";
+import Comet from "./Comet.tsx";
 import Orbit from "./Orbit.tsx";
+import Ufo from "./UFO.tsx";
 
 const App = () => {
     return <div id="root">
         <Orbit />
+        <Comet />
+        <Ufo />
     </div>
 }
 
---
+++ frontend/components/Ufo.tsx
@@ -0,0 +1,7 @@
+import { React } from "../dep.ts";
+
+const Ufo = () => {
+    return <img src="/static/images/ufo.png" id="ufo" alt="ufo" />
+}
+
+export default Ufo;
